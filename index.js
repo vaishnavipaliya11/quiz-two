bd6621d30ff286cbb995d08aa629ba9e8a5fdb11
vaishnavi paliya <paliyavaishnavi11@gmail.com>
2021-04-16 11:41:35 +0530

    feat: adding list of ques ans arrays.

--- index.js
+++ index.js
@@ -12,3 +12,34 @@ console.log('3. One Point will be deducted if the Answer is Wrong.');
 console.log('4. In MCQ based questions you have to type the Serial Number / Index Value.');
 console.log('all the best!!!!')
 console.log('\n');
+
+var score=0
+
+var mcqList = [
+
+  {
+    array : ['pandava', 'arjuna', 'krishna', 'drihastara'],
+    question : 'who is duryodhan father? ',
+    answer : 'drihastara'
+  },
+  {
+    array : ['sanskrit', 'marathi', 'hindi ', 'punjabi'],
+    question : 'what is lamgauge of gita',
+    answer : 'sanskrit'
+  },
+  {
+    array : ['rajas','tamas','yoga','saatva'],
+    question : 'which guna is associated with passion?',
+    answer : 'rajas'
+  },
+  {
+    array : ['710', '750', '100', '700'],
+    question : 'how many verses bhagvad gita has? ',
+    answer : '700'
+  },
+  {
+    array : ['less people follow dharma', 'there are more bhajans', 'more people follow dharma', 'more bhakti'],
+    question : 'lord KRISHNA says that he takes avtaras whwnever  ',
+    answer : 'less people follow dharma'
+  },
+];
