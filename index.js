60ba3892cb1d9b64cc6a3199538098827b9a3e0d
vaishnavi paliya <paliyavaishnavi11@gmail.com>
2021-04-16 11:33:40 +0530

    docs: added rules for the quiz.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 const readlineSync= require('readline-sync')
+
+console.log('Welcome to the Quiz');
+let username = readlineSync.question("What's your Name: ");
+console.log('Hello',username,", Let's Play the Quiz!!");
+
+console.log('\n');
+console.log('Rules & Instructions: ');
+console.log('1.',username + ', There are 10 Questions on India and all are Compulsory.');
+console.log('2. You will get 1 points on each Right Answer.');
+console.log('3. One Point will be deducted if the Answer is Wrong.');
+console.log('4. In MCQ based questions you have to type the Serial Number / Index Value.');
+console.log('all the best!!!!')
+console.log('\n');
